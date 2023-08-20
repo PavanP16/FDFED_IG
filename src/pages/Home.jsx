162c3565bda0dd708d3ad872fa722b156299c5af
Rahul <rahul.red1801@gmail.com>
2023-08-20 14:42:03 +0530

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,6 @@ const Home = () => {
   return (
     <div>
       Home
-      Changed by Rahul
-      Second modify by pavan
-      Damn
-      First push try
-      Second push try
     </div>
   )
 }
